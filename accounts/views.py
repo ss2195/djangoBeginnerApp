d7c2a68102cfc76905420c983a3492fba7537a11
ss2195 <shubhamshourya1995@gmail.com>
2021-01-25 19:50:24 +0530

    Accounts py file comitted

---
+++ accounts/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+
+# Create your views here.
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/")
+        else:
+            messages.info(request,"Check your usename or password")
+            return redirect('login.html')
+            
+
+    else:
+        return render(request,'login.html')
+
+
+def register(request):
+    if request.method == "POST":
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        email = request.POST["email"]
+        username = request.POST["username"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,"Username taken")
+                return redirect("register")
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,"Email taken")
+                return redirect("register")
+            else:
+                user = User.objects.create_user(username = username,password = password1,email = email, first_name=first_name,last_name=last_name)
+                user.save()
+                messages.info(request,"User Created")
+                return redirect("login")
+            
+
+        else:
+            messages.info(request,"Password Not matching")
+            
+
+    else:
+        return render(request,'register.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("/")
