efcd5253191a987a0fbda8b35a343c8e8f2634aa
ss2195 <shubhamshourya1995@gmail.com>
2021-01-25 19:53:28 +0530

    Calc app

---
+++ calc/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html',{'name':"Shubham"})
+
+def add(request):
+    val1 = request.POST["num1"]
+    val2 = request.POST["num2"]
+    res = float(val1)+float(val2)
+    return render(request, 'result.html',{"result":res})
